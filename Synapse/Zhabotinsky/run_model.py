98b16d051f7809eb2210464b677ef1a0c782ffeb
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-07 10:42:49 +0530

    Get Zhabotinsky working. Added missing scripts.

---
+++ Synapse/Zhabotinsky/run_model.py
@@ -0,0 +1,25 @@
+"""run_model.py: 
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2015, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+sys.path.append('../../..')
+import yacml
+import moose
+
+def main():
+    modelfile = sys.argv[1]
+    model = yacml.loadYACML(modelfile)
+    model.run()
+
+if __name__ == '__main__':
+    main()
