147480b089f91c2c4658fe40b7b085033357f6c0
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-08 18:58:47 +0530

    Implemented setting concentration when rate is given.

--- Synapse/Zhabotinsky/run.sh
+++ Synapse/Zhabotinsky/run.sh
@@ -4,10 +4,13 @@ set -x
 MODEL_FILE="$1"
 IMAGE_DIR=./_images/`date +%F`
 FILE_NAME_TIMESTAMPED="$IMAGE_DIR/$MODEL_FILE_`date +%R`.png"
-
+PY=python
 mkdir -p $IMAGE_DIR
 
-python ./run_model.py "$MODEL_FILE"
+if [[ "$2" == "-x" ]]; then
+    PY="gdb -ex r --args python"
+fi
+$PY ./run_model.py "$MODEL_FILE"
 ~/Scripts/plot_csv.py -i "$MODEL_FILE".dat -y 1-10 -s -o $MODEL_FILE.png
 
 echo "Copying image to $FILE_NAME_TIMESTAMPED"
