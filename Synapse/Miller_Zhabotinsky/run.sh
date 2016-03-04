0f1ea79e5a2e6bb2a9405906fe35ea6aa8f5d465
Dilawar Singh <dilawars@ncbs.res.in>
2016-03-04 17:49:29 +0530

    This is cool. Now move to branch. A system of CaMKII = 4, PP1 = 1 molecules showing bistable behaviour.

--- Synapse/Miller_Zhabotinsky/run.sh
+++ Synapse/Miller_Zhabotinsky/run.sh
@@ -84,7 +84,7 @@ $PYTHON ./run_model.py "$MODEL_FILE"
 # Plot only if not in debug mode.
 if [ ! $DO_DEBUG ]; then 
     ~/Scripts/plot_csv.py -i "$MODEL_FILE".dat \
-        -y "PP1.N,PP1.conc,PP1_.N,x0.N,x1.N,x2.N,x6.N,ca.conc" \
+        -y "PP1_.N,PP1.N,x0.N,x6.N,I1P.conc,pqf.N,ca.conc" \
         -s -o $MODEL_FILE.png
     echo "Copying image to $FILE_NAME_TIMESTAMPED".png
     cp $MODEL_FILE.png $FILE_NAME_TIMESTAMPED.png
